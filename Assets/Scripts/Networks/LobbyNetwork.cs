bd20ba8a8014b5d37da37100d5cb62231f9bfa15
feltos <guillaumevolgyesi@gmail.com>
2020-10-01 02:10:44 +0200

    lobby created

---
+++ Assets/Scripts/Networks/LobbyNetwork.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+
+public class LobbyNetwork : PunBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("connecting to server...");
+        PhotonNetwork.ConnectUsingSettings("0.0.0");
+    }
+    public override void OnConnectedToMaster()
+    {
+        print("Connected to Master");
+
+        PhotonNetwork.playerName = PlayerNetwork.Instance.playerName;
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("Joined Lobby");
+    }
+
+}
---
+++ Assets/Scripts/Networks/PlayerNetwork.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerNetwork : MonoBehaviour
+{
+    public static PlayerNetwork Instance;
+    public string playerName { get; private set; }
+    private void Awake()
+    {
+        Instance = this;
+
+        name = "Player#" + Random.Range(1000,9999);
+    }
+
+}
