c37bde1a8373da86e9d54912fb66f3e8c65c4e5f
guillaume volgyesi <guillaumevolgyesi@gmail.com>
2021-01-14 20:48:06 +0100

    add player name in game

---
+++ Assets/Scripts/Player/PlayerNameTag.cs
@@ -0,0 +1,27 @@
+﻿using Photon;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameTag : Photon.MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI nameText; 
+    void Start()
+    {
+        if (photonView.isMine)
+        {
+            return;
+        }
+
+        SetName();
+    }
+
+    private void SetName()
+    {
+        nameText.text = photonView.owner.NickName;
+    }
+}
